66168fd172519726c6ecdd12f2ab5f4773209fd8
winsznx <timjosh507@gmail.com>
2026-04-16 20:31:17 +0100

    feat(frontend): add shared UI size and variant constants

---
+++ frontend/src/constants/variants.js
@@ -0,0 +1,28 @@
+export const SIZES = Object.freeze(['sm', 'md', 'lg', 'xl'])
+export const DEFAULT_SIZE = 'md'
+
+export const BUTTON_VARIANTS = Object.freeze([
+  'primary',
+  'secondary',
+  'outline',
+  'ghost',
+  'danger',
+])
+export const DEFAULT_BUTTON_VARIANT = 'primary'
+
+export const BADGE_VARIANTS = Object.freeze([
+  'neutral',
+  'success',
+  'warning',
+  'danger',
+  'info',
+])
+export const DEFAULT_BADGE_VARIANT = 'neutral'
+
+export const ALERT_VARIANTS = Object.freeze([
+  'info',
+  'success',
+  'warning',
+  'danger',
+])
+export const DEFAULT_ALERT_VARIANT = 'info'
