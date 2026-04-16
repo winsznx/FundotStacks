223295ef392f73b0e96adc5438e8112b57171f0b
winsznx <timjosh507@gmail.com>
2026-04-16 20:31:20 +0100

    feat(frontend): add barrel export for common components

---
+++ frontend/src/components/common/index.js
@@ -0,0 +1,4 @@
+export { ErrorAlert } from './ErrorAlert.jsx'
+export { ErrorBoundary } from './ErrorBoundary.jsx'
+export { LoadingSpinner } from './LoadingSpinner.jsx'
+export { TransactionStatus } from './TransactionStatus.jsx'
